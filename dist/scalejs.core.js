629362ed6491ff431396daa4c4bd2d88c7d7e3e5
Draisy <draisy.sabel@eikospartners.com>
2016-05-31 16:45:40 -0400

    update export syntax

--- dist/scalejs.core.js
+++ dist/scalejs.core.js
@@ -1,9 +1,13 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _scalejsEs = require("./scalejs.es6.core");
 
 var _scalejsEs2 = _interopRequireDefault(_scalejsEs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = _scalejsEs2.default;
+exports.default = _scalejsEs2.default;
--- src/scalejs.core.js
+++ src/scalejs.core.js
@@ -1,2 +1,2 @@
 import core from "./scalejs.es6.core";
-module.exports = core;
+export default core;
